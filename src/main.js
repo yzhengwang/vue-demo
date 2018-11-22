7fcefdd92cabb207cf39e4770f6ce0a9d692c0b8
yzhengwang <1402205524@qq.com>
2018-11-22 17:33:14 +0800

    添加vue-demo代码
    Signed-off-by: yzhengwang <1402205524@qq.com>

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 引入jQuery
+import $ from 'jquery'
+// 引入bootstrap
+import './assets/css/bootstrap.min.css'
+import './assets/js/bootstrap.min'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    components: {
+        App
+    },
+    template: '<App/>'
+})
